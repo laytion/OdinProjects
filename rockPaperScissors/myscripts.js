ced62df8a913055f377d5aa68754844d6a9986b0
laytion <talshijira@gmail.com>
2021-08-30 17:40:19 +0700

    created ui for rps

--- rockPaperScissors/myscripts.js
+++ rockPaperScissors/myscripts.js
@@ -1,49 +1,87 @@
 function computerPlay() {
     let play = Math.floor(Math.random() * 3);
     if (play == 0) {
-        return 'rock'
+        return 'rock';
     }
     else if (play == 1) {
-        return 'paper'
+        return 'paper';
     }
-    else return 'scissors'
+    else return 'scissors';
 }
 
-// let playerSelection = prompt("What will you play?").toLowerCase();
-function oneRound(computerSelection) {
-    let playerSelection = prompt("What will you play?").toLowerCase();
+// takes in computer's and player's choices and returns 'win' or lose'
+function oneRound(computerSelection, playerSelection) {
+    let resultStr = '';
+
     if (playerSelection == computerSelection) {
-        console.log('draw')
-        // return 'draw';
+        resultStr = 'draw';
     }
     else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('lose')
-        // return 'lose';
+        resultStr = 'lose';
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('win')
-        // return 'win!';
+        resultStr = 'win';
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('win')
-        // return 'win!';
+        resultStr = 'win';
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        console.log('lose')
-        // return 'lose';
+        resultStr = 'lose';
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        console.log('lose')
-        // return 'lose';
+        resultStr = 'lose';
     }
     else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('win')
-        // return 'win!';
+        resultStr = 'win';
     }
+
+    const results = document.querySelector('#results');
+    results.textContent = resultStr;
+    return resultStr;
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        oneRound(computerPlay());
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const startBtn = document.querySelector('#start');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const winner = document.querySelector('#winner');
+let win = 0;
+let lose = 0;
+
+rockBtn.addEventListener('click', () => {
+    const computer = computerPlay();
+    oneRound(computer, 'rock');
+    calculateScore(oneRound(computer, 'rock'));
+})
+
+paperBtn.addEventListener('click', () => {
+    const computer = computerPlay();
+    oneRound(computer, 'paper');
+    calculateScore(oneRound(computer, 'paper'));
+})
+
+scissorsBtn.addEventListener('click', () => {
+    const computer = computerPlay();
+    oneRound(computer, 'scissors');
+})
+
+// takes in 'win' or 'lose' or 'draw' and increments scores
+function calculateScore(result) {
+    if (result == 'win') {
+        win += 1;
+        playerScore.textContent = `Player Score: ${win}`;
+    }
+    else if (result == 'lose') {
+        lose += 1;
+        computerScore.textContent = `Computer Score: ${lose}`;
+    }
+
+    if (win == 5) {
+        winner.textContent = "PLAYER WINS";
+    }
+    else if (lose == 5) {
+        winner.textContent = "COMPUTER WINS";
     }
 }
